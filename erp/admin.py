a3bb4043edd960b2eab1ba09eb2be018ff8ff1f6
techjob <alan.albiero@gmail.com>
2024-07-15 10:47:19 -0300

    Criado novas views e ativado Admin.

--- erp/admin.py
+++ erp/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
-# Register your models here.
+from erp.models import Funcionario, Produto, Venda
+
+
+class FuncionarioAdmin(admin.ModelAdmin):
+    pass
+
+
+class ProdutoAdmin(admin.ModelAdmin):
+    pass
+
+
+class VendaAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Funcionario, FuncionarioAdmin)
+admin.site.register(Produto, ProdutoAdmin)
+admin.site.register(Venda, VendaAdmin)
